5ec908a0f3d8c2f4222a392105058f161f122425
Oluwasegun Ajayi <ajayimooseize@gmail.com>
2021-06-20 06:49:56 +0100

    passport

--- server.js
+++ server.js
@@ -1,24 +1,36 @@
-'use strict';
-require('dotenv').config();
-const express = require('express');
-const myDB = require('./connection');
-const fccTesting = require('./freeCodeCamp/fcctesting.js');
-const path = require('path')
+"use strict";
+require("dotenv").config();
+const express = require("express");
+const myDB = require("./connection");
+const fccTesting = require("./freeCodeCamp/fcctesting.js");
+const path = require("path");
+const passport = require("passport");
+const session = require("express-session");
 
 const app = express();
 
 fccTesting(app); //For FCC testing purposes
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.set('view engine', 'pug')
-app.set('views', path.join(__dirname, '/views/pug'));
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "/views/pug"));
 
-app.route('/').get((req, res) => {
-  res.render('index', {title: 'Hello', message: 'Please login'});
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
+app.use(passport.initialize());
+
+app.route("/").get((req, res) => {
+  res.render("index", { title: "Hello", message: "Please login" });
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log('Listening on port ' + PORT);
+  console.log("Listening on port " + PORT);
 });
