3cae8b2be7689d0923585387b00a00ea65a83aef
Oluwasegun Ajayi <ajayimooseize@gmail.com>
2021-06-20 06:42:00 +0100

    Initial Commit

---
+++ server.js
@@ -0,0 +1,24 @@
+'use strict';
+require('dotenv').config();
+const express = require('express');
+const myDB = require('./connection');
+const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const path = require('path')
+
+const app = express();
+
+fccTesting(app); //For FCC testing purposes
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, '/views/pug'));
+
+app.route('/').get((req, res) => {
+  res.render('index', {title: 'Hello', message: 'Please login'});
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log('Listening on port ' + PORT);
+});
