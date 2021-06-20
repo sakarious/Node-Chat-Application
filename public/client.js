c8f55cc8333f37bd3a8e064692e3d94193c75518
Oluwasegun Ajayi <ajayimooseize@gmail.com>
2021-06-20 12:48:45 +0100

    chat

--- public/client.js
+++ public/client.js
@@ -10,6 +10,11 @@ $(document).ready(function () {
     $("#messages").append($("<li>").html("<b>" + message + "</b>"));
   });
 
+  socket.on("chat message", (data) => {
+    console.log("socket.on 1");
+    $("#messages").append($("<li>").text(`${data.name}: ${data.message}`));
+  });
+
   // Form submittion with new message in field with id 'm'
   $("form").submit(function () {
     let messageToSend = $("#m").val();
