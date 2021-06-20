232f7a2bbe9272dc0ea8e2f3b51c301aa1bbd2c9
Oluwasegun Ajayi <ajayimooseize@gmail.com>
2021-06-20 08:22:52 +0100

    logout

--- server.js
+++ server.js
@@ -70,6 +70,15 @@ myDB(async (client) => {
     res.render("profile", { username: req.user.username });
   });
 
+  app.route("/logout").get((req, res) => {
+    req.logout();
+    res.redirect("/");
+  });
+
+  app.use((req, res, next) => {
+    res.status(404).type("text").send("Not Found");
+  });
+
   passport.use(
     new LocalStrategy(function (username, password, done) {
       myDataBase.findOne({ username: username }, function (err, user) {
